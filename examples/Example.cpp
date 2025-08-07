2a9a0e0f5e572cdf1a8f52966e900f230aa9477a
Seno Alvrtsyan <alvrtsyan.seno@gmail.com>
2025-08-07 14:04:18 +0400

    Update Example.cpp

--- examples/Example.cpp
+++ examples/Example.cpp
@@ -8,19 +8,27 @@
 int main() {
 
     openshot::Timeline timeLine(1920, 1080, openshot::Fraction(30, 1), 48000, 2, openshot::ChannelLayout::LAYOUT_STEREO);
+
     timeLine.Open();
 
-    auto reader = openshot::FFmpegReader("be2372bf-27e8-4bcd-bfd7-620a68496267");
-    openshot::Clip c(&reader);
-    timeLine.AddClip(&c);
+
+    const auto clip = new openshot::Clip("img.jpeg");
+    timeLine.AddClip(clip);
+
+    // Load from JSON file (alternative to programmatic creation)
+    timeLine.LoadSubtitlesFromJsonFile("./subtitles.json");
 
     openshot::FFmpegWriter w("out.mp4");
     w.SetAudioOptions(true, "aac", 48000, 2, openshot::ChannelLayout::LAYOUT_STEREO, 128000);
-    w.SetVideoOptions(true, "libx264" , openshot::Fraction(30, 1),  1920, 1080, openshot::Fraction(1,1), false, false, 4000000);
+    w.SetVideoOptions(true, "libx264" , openshot::Fraction(30, 1),  1280, 720, openshot::Fraction(1,1), false, false, 4000000);
+
+    w.PrepareStreams();
+    w.SetOption(openshot::VIDEO_STREAM, "crf", "18");
+    w.SetOption(openshot::VIDEO_STREAM, "preset", "medium");
+    w.SetOption(openshot::VIDEO_STREAM, "x264-params", "colorprim=bt709:transfer=bt709:colormatrix=bt709");
     w.Open();
 
-
-    w.WriteFrame(&timeLine, 1, 29 * 30);
+    w.WriteFrame(&timeLine, 1, 15*30);
     timeLine.Close();
     w.Close();
     return 0;
