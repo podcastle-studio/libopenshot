7bca3399fff34b1709219f17a7ab6f446ec537f2
Seno Alvrtsyan <alvrtsyan.seno@gmail.com>
2025-08-21 15:43:13 +0400

    Update image processing lib

--- examples/Example.cpp
+++ examples/Example.cpp
@@ -5,14 +5,13 @@
 #include "effects/ColorAdjustment.h"
 
 int main() {
-    constexpr int renderWidth = 480;
-    constexpr int renderHeight = 480;
+    constexpr int renderWidth = 720;
+    constexpr int renderHeight = 720;
     openshot::Timeline timeLine(renderWidth, renderHeight, openshot::Fraction(30, 1), 48000, 2, openshot::ChannelLayout::LAYOUT_STEREO);
 
     timeLine.Open();
 
-    const auto clip = new openshot::Clip("img.jpeg");
-    clip->scale = openshot::SCALE_STRETCH;
+    const auto clip = new openshot::Clip("img");
     timeLine.AddClip(clip);
 
     // Load from JSON file (alternative to programmatic creation)
@@ -28,7 +27,7 @@ int main() {
     w.SetOption(openshot::VIDEO_STREAM, "x264-params", "colorprim=bt709:transfer=bt709:colormatrix=bt709");
     w.Open();
 
-    w.WriteFrame(&timeLine, 1, 6*30);
+    w.WriteFrame(&timeLine, 1, 20*30);
     timeLine.Close();
     w.Close();
     return 0;
